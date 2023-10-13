7880a7996aa9102431da7169e6df8cd597e427b4
ConstasJ <constasj@qq.com>
2023-10-13 13:03:10 +0800

    feat: add type safe event emitter and handler for Client

--- src/clients/Client.ts
+++ src/clients/Client.ts
@@ -1,6 +1,7 @@
 import { Socket } from 'net';
 import SimpleClient from './SimpleClient';
 import { Protocol } from '../utils';
+import { BroadcastMessage, SendChatMessage, SendListMessage } from '../types';
 
 export default class Client extends SimpleClient {
     public entry: Protocol;
@@ -19,5 +20,26 @@ export default class Client extends SimpleClient {
         this.entry = entry;
         this.socket = socket;
         this.lastPulseTime = lastPulseTime;
+    }
+
+    public on(event: 'chat', listener: (msg: SendChatMessage) => void): this;
+    public on(event: 'broadcast', listener: (msg: BroadcastMessage) => void): this;
+    public on(event: 'list', listener: (msg: SendListMessage) => void): this;
+    public on(event: string, listener: (msg: any) => void): this {
+        return super.on(event, listener);
+    }
+
+    public once(event: 'chat', listener: (msg: SendChatMessage) => void): this;
+    public once(event: 'broadcast', listener: (msg: BroadcastMessage) => void): this;
+    public once(event: 'list', listener: (msg: SendListMessage) => void): this;
+    public once(event: string, listener: (msg: any) => void): this {
+        return super.on(event, listener);
+    }
+
+    public emit(event: 'chat', msg: SendChatMessage): any;
+    public emit(event: 'broadcast', msg: BroadcastMessage): any;
+    public emit(event: 'list', msg: SendListMessage): any;
+    public emit(event: string, msg: any) {
+        return super.emit(event, msg);
     }
 }
